494995cb38f16ddc46d91121482e319a6d35eb00
Saul Piedra <89549378+SauloPiedra@users.noreply.github.com>
2022-01-21 16:03:17 -0600

    Add files via upload

---
+++ P6_LidDriven/utils.f90
@@ -0,0 +1,60 @@
+!:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+subroutine interpolateToNodesUs(uc,us,nx,ny)
+
+implicit none
+integer nx,ny
+real:: us(0:nx,0:ny+1),uc(0:nx+1,0:ny+1)
+integer bi,ei,bj,ej,i,j
+bi=1; bj=1;ej=ny; ei=nx
+
+! Internal points
+do i=bi,ei
+do j=bj-1,ej+1
+	uc(I,J) = ( us(I-1,J) + us(I,J) ) * 0.5
+end do 
+end do
+
+uc(bi-1,:)=us(0,:)
+uc(ei+1,:)=us(nx,:)
+
+end subroutine
+
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+subroutine interpolateToNodesVs(vc,vs,nx,ny)
+implicit none
+integer nx,ny
+real:: vs(0:nx+1,0:ny),vc(0:nx+1,0:ny+1)
+integer bi,ei,bj,ej,i,j
+bi=1; bj=1;ej=ny; ei=nx
+
+do i=bi-1,ei+1
+do j=bj,ej
+	vc(I,J) = ( vs(I,J) + vs(I,J-1) ) * 0.5
+end do 
+end do
+
+vc(:,bj-1)=vs(:,0)
+vc(:,ej+1)=vs(:,ny)
+end subroutine
+
+!:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Subroutine WriteVectorField(Name,kx,uc,vc,xc,yc,nx,ny)
+integer i,j,nx,ny,kx
+real*4 uc(0:nx+1,0:ny+1),vc(0:nx+1,0:ny+1),xc(0:nx+1),yc(0:ny+1)
+character*(*)Name
+character*50 txt,Filename
+write(txt,'(i6)')kx
+txt=ADJUSTL(txt)
+Filename=name//txt(1:len_trim(txt))//".txt"
+
+open(11,file=Filename(1:len_trim(Filename)))
+	do i=0,nx+1
+	do j=0,ny+1
+	write(11,*)xc(i),yc(j),uc(i,j),vc(i,j)
+	end do
+	write(11,*)''
+	end do
+close(11)
+End Subroutine
